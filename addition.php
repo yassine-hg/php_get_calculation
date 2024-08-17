82ce8d51cbad3c4ede445af78b760ba883c592ef
yassine-hg <160339862+yassine-hg@users.noreply.github.com>
2024-08-17 17:39:23 +0100

    Create addition.php

---
+++ addition.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+  <?=
+    "The sum of ${_GET['add1']} and ${_GET['add2']} is" 
+  ?>
+  <h4>
+    <?= $_GET['add1'] + $_GET['add2'] ?> 
+  </h4>
+  <br>
+<a href="index.php">Reset</a>
+</body>
+</html>
