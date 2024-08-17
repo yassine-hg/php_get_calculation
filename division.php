fff6adf0bf814232f012210c7ad41626f4f1cc11
yassine-hg <160339862+yassine-hg@users.noreply.github.com>
2024-08-17 17:39:55 +0100

    Create division.php

---
+++ division.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+  <?=
+    "the divition between ${_GET['div1']} and ${_GET['div2']} is" 
+  ?>
+  <br>
+  <h4>
+    <?=
+      $_GET['div1'] / $_GET['div2']
+    ?>
+  <h4>
+<a href="index.php">Reset</a>
+</body>
+</html>
