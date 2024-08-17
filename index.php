9c4b7581d96c813b5eed76685996f196984bfc58
yassine-hg <160339862+yassine-hg@users.noreply.github.com>
2024-08-17 17:38:58 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<h2>Addition</h2>
+<form action="addition.php" method="get">
+  Enter the first number: <input type ="number" name="add1"><br><br>
+  Enter the second number: <input type = "number" name="add2"><br><br>
+  <button type ="submit">Add</button>
+</form>
+<h2>Divition</h2>
+<form action="division.php" method="get">
+  Enter the numerator <input type ="number" name="div1" ><br><br>
+  Enter the denominator <input type ="number" name="div2"><br><br>
+  <button type ="submit">Divide</button>
+</form>
+<a href="index.php">Reset</a>
+</body>
+</html>
